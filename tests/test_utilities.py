6d8c2f1cc00a1e0a3f2506430e0e43a672d0a82c
George Richardson <georgerichardson@posteo.net>
2018-08-20 13:13:22 +0100

    add test for flatten

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,7 +2,7 @@ import pytest
 
 from rhodonite.utilities import edge_property_as_matrix, flatten, window
 
-def test_flatten():
+def test_flatten_list_of_lists():
     nested = [
 	    ['a', 'b', 'c'],
 	    ['d','e'],
