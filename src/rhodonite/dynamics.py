51b8d4b42ec35874cab3130e159d52a5bb255505
George Richardson <georgerichardson@posteo.net>
2018-08-23 12:10:27 +0100

    add build method for phylomemetic graph

--- src/rhodonite/dynamics.py
+++ src/rhodonite/dynamics.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 from graph_tool.all import Graph, GraphView
+from operator import itemgetter
 
-from rhodonite.cliques import find_cliques_cfinder
+from rhodonite.cliques import find_cliques_cfinder, clique_unions
 from rhodonite.similarity import jaccard
 
 
@@ -16,6 +17,7 @@ class PhylomemticGraph(Graph):
             self.weights = [g.edge_properties[weights] for g in self.graphs]
         else:
             self.weights = weights
+        self.dictionary = dictionary
         self.names = names
         self.normalisation_weight = normalisation_weight
         self.delta = delta
@@ -23,6 +25,8 @@ class PhylomemticGraph(Graph):
         self.max_weight = max_weight
 
         self.new_vertex_property('vector<int>')
+
+        self.n_cooccurrence_vertices = len(dictionary.keys())
 
     def prepare(self, cfinder_path, clique_output_dir):
         """prepare
@@ -40,24 +44,41 @@ class PhylomemticGraph(Graph):
             self.clique_sets.append(clique_sets)
 
     def build(self):
+        """build
+        Creates the links in the phylomemetic network between cliques at time T
+        and time T'.
+        """
         
-        phylomemetic_links = []
-
         for cliques_past, cliques_future in window(self.clique_sets, 2):
+            phylomemetic_links = []
+
             self.add_vertex(len(cliques_past))
             self.add_edge_list(phylomemetic_links)
+            
+            future_vertices_start = len(list(self.vertices()))
+            
 
-            overlapping_past_cliques = []
+            cp_union_indices, cp_union_vertices = clique_unions(cliques_past)
+            cp_matrix = np.zeros(
+                    (len(cp_union_vertices), self.n_cooccurences_vertices))
+            for i, cuv in enumerate(cp_union_vertices):
+                rows = [i] * len(cuv)
+                cp_matrix[rows, cuv]
 
-            cliques_past_unions = 
+            for i, cf in enumerate(cliques_future):
+                future_clique_vertex = i + future_vertices_start
 
-            for cf in cliques_future:
-                jaccard_matches = []
-                for cp in cliques_past:
-                    if not set(cf).isdisjoint(cp):
-                        j = jaccard(cf, cp)
-                        if j >= self.delta:
-                            jaccard_matches.append(j)
-                        else:
-                            jaccard_matches.append(0)
+                cf_vector = np.zeros(len(self.n_cooccurrence_vertices))
+                cf_vector = np.put(cf_vector, cf, 1)
+                jaccard_similarities = jaccard_similarity(cf_vector, cp_matrix)
+
+                if np.max(jaccard_similarities) >= self.delta:
+                    parent_clique_indices = flatten(np.argwhere(
+                        jaccard_similarities == np.max(jaccard_similarities)))
+
+                        parent_cliques = flatten(
+                                itemgetter(*parent_clique_indices)(cliques_past))
+                        for pc in parent_cliques:
+                            phylomemetic_links.append((pc, future_clique_vertex))
+
 
