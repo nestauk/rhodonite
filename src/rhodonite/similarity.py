ffd8b89dc6665efe2569b67b9dd30922a186a984
George Richardson <georgerichardson@posteo.net>
2018-08-30 10:10:23 +0100

    change jaccard to handle sparse matrices

--- src/rhodonite/similarity.py
+++ src/rhodonite/similarity.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix, csc_matrix
 
-def jaccard_similarity(a, B):
+def jaccard_similarity(A, B):
     """jaccard_similarity
     Calculates the jaccard similarity between a vector a and all of the rows in
     a matrix B.
@@ -14,15 +14,18 @@ def jaccard_similarity(a, B):
         j (:obj:`np.array`): A vector containing the row-wise jaccard
             similarities between a and B.
     """
-    if isinstance(B, (csr_matrix, csc_matrix)):
-        l = B.shape[0]
-        A = np.tile(a.todense(), [l, 1])
-    else:
-        l = len(B)
-        A = np.tile(a, [l, 1])
- 
-    intersection = np.sum(np.multiply(A, B), axis=1)
-    union = np.sum(((A + B) > 0), axis=1)
+    intersection = A.multiply(B).sum(axis=1)
+    union = ((A + B) > 0).sum(axis=1)
     j = np.divide(intersection, union)
+#     if isinstance(B, (csr_matrix, csc_matrix)):
+#         l = B.shape[0]
+#         A = np.tile(a.todense(), [l, 1])
+#     else:
+#         l = len(B)
+#         A = np.tile(a, [l, 1])
+#  
+#     intersection = np.sum(np.multiply(A, B), axis=1)
+#     union = np.sum(((A + B) > 0), axis=1)
+#     j = np.divide(intersection, union)
     return j
 
