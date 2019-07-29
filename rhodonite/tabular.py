977cf437592f4de563277d222b9272d3a7165c6d
George Richardson <georgerichardson@posteo.net>
2019-07-29 15:30:54 +0100

    select columns for tabular exports

--- rhodonite/tabular.py
+++ rhodonite/tabular.py
@@ -24,20 +24,23 @@ def vertices_to_dataframe(g, vectors=False, keys=None):
     else:
         filtered=False
         filt = np.array([True] * g.num_vertices())
+    if keys is None:
+        keys = list(g.vp.keys())
     for k, vp in g.vp.items():
-        vt = vp.value_type()
-        if ('vector' not in vt) & ('string' not in vt) & ('object' not in vt):
-            if ('int' in vt) | ('bool' in vt):
-                vertex_df[k] = vp.get_array()[filt]
-                vertex_df[k] = vertex_df[k].astype(int)
-            elif 'double' in vt:
-                vertex_df[k] = vp.get_array()[filt]
-                vertex_df[k] = vertex_df[k].astype(float)
-        elif ('vector' in vt) & ('string' not in vt) & (vectors == True):
-            vertex_df[k] = [[i for i in vp[v]] for v, f in zip(g.vertices(), filt) if f]
+        if k in keys:
+            vt = vp.value_type()
+            if ('vector' not in vt) & ('string' not in vt) & ('object' not in vt):
+                if ('int' in vt) | ('bool' in vt):
+                    vertex_df[k] = vp.get_array()[filt]
+                    vertex_df[k] = vertex_df[k].astype(int)
+                elif 'double' in vt:
+                    vertex_df[k] = vp.get_array()[filt]
+                    vertex_df[k] = vertex_df[k].astype(float)
+            elif ('vector' in vt) & ('string' not in vt) & (vectors == True):
+                vertex_df[k] = [[i for i in vp[v]] for v, f in zip(g.vertices(), filt) if f]
     return vertex_df
 
-def edges_to_dataframe(g, sort=None, vectors=False):
+def edges_to_dataframe(g, keys=None, drop_keys=[], sort=None, vectors=False):
     """edges_to_dataframe
     Transforms a graph's edges and their properties into a tabular format.
 
@@ -55,17 +58,20 @@ def edges_to_dataframe(g, sort=None, vectors=False):
     edge_df = pd.DataFrame(list(g.edges()), columns=['s', 't'], dtype='int')
     edge_df = pd.DataFrame(g.get_edges(), columns=['s', 't', 'e_index'], dtype='int')
     indices = edge_df['e_index'].values
-    for k, ep in g.ep.items():    
-        vt = ep.value_type()
-        if ('vector' not in vt) & ('string' not in vt) & ('object' not in vt):
-            if ('int' in vt) | ('bool' in vt):
-                edge_df[k] = ep.get_array()[indices]
-                edge_df[k] = edge_df[k].astype(int)
-            elif 'double' in vt:
-                edge_df[k] = ep.get_array()[indices]
-                edge_df[k] = edge_df[k].astype(float)
-        elif ('vector' in vt) & ('string' not in vt) & (vectors == True):
-            edge_df[k] = [[i for i in vp[v]] for v in g.vertices()]
+    if keys is None:
+        keys = list(g.ep.keys())
+    for k, ep in g.ep.items():
+        if k in keys:
+            vt = ep.value_type()
+            if ('vector' not in vt) & ('string' not in vt) & ('object' not in vt):
+                if ('int' in vt) | ('bool' in vt):
+                    edge_df[k] = ep.get_array()[indices]
+                    edge_df[k] = edge_df[k].astype(int)
+                elif 'double' in vt:
+                    edge_df[k] = ep.get_array()[indices]
+                    edge_df[k] = edge_df[k].astype(float)
+            elif ('vector' in vt) & ('string' not in vt) & (vectors == True):
+                edge_df[k] = [[i for i in vp[v]] for v in g.vertices()]
     if sort:
         edge_df.sort_values(sort, inplace=True)
     return edge_df
